2e2c326ec20aa2b28d8338e13aa9af412c4e812e
rvila94 <96233869+rvila94@users.noreply.github.com>
2025-04-15 17:54:57 +0200

    Revert commit

--- src/module-info.java
+++ src/module-info.java
@@ -1,8 +1,7 @@
 module ErrorShooter {
 	requires javafx.controls;
 	requires javafx.graphics;
-	// DECOMENTE SI TU VEUX LES TESTS JUNIT
-	//requires org.junit.jupiter.api;
+	requires org.junit.jupiter.api;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
